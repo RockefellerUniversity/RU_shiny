c46344260f858957ca05a1484d001b0ad10d5aa7
Douglas Barrows <dbarrows@rockefeller.edu>
2025-04-07 16:57:28 -0400

    have gone through one and two

---
+++ RUshiny/inst/extdata/data/app_debug.R
@@ -0,0 +1,28 @@
+library(shiny)
+library(bslib)
+library(ggplot2)
+library(DT)
+library(rio)
+library(plotly)
+
+ui <- page_fluid(
+  fileInput("de_file", "Upload a DE file", accept = c(".csv", ".tsv", "xlsx", "xls")), 
+  
+  uiOutput("all_data_UI")
+)
+
+server <- function(input, output){
+  de_table_in <- reactive({
+    req(input$de_file)
+    print(input$de_file)
+    rio::import(input$de_file) %>% dplyr::mutate(negLog10_pval = -log10(pvalue))})
+  
+  output$all_data_UI <- renderUI({ 
+    req(de_table_in)
+    card(card_header("All genes"), dataTableOutput(outputId = "all_data"))
+  }) 
+  
+  output$all_data = renderDataTable(datatable(de_table_in()))
+}
+
+shinyApp(ui = ui, server = server)
